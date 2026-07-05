2bbc5bdc32ffbc16351fd95390c7a9743931be45
ImarkUwu <markitospere12@gmail.com>
2026-07-05 15:01:52 -0400

    Revert "-Creacion de la carpeta "Scripts""
    This reverts commit 24b06712851293397ef99717db156ecdec8a4e80.

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -12,7 +12,6 @@ namespace SistemaEmergenciaHospitalaria.Datos
     {
         private SqlConnection conexion = new SqlConnection(
             "Server=JORDINOVAS\\MSSQLSERVER02;Database=SistemaEmergenciaHospitalaria;Trusted_Connection=True;TrustServerCertificate=True;"
-            //"Server=localhost\\SQL2025;Database=SistemaEmergenciaHospitalaria;User Id=sa;Password=123456;TrustServerCertificate=True;"
         );
 
         public SqlConnection AbrirConexion()
